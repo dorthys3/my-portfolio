a9043d52865f8b4b0834ce4fc0d030fa8cec128e
DorthyChristobel <dorthychristobel30@gmail.com>
2025-09-24 16:41:02 +0530

    Updated index.html, data.js, styles and added new project images

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
 // src/data.js
 import project1 from "./assets/project1.png.png";
 import project2 from "./assets/project2.png.png";
-
+import project3 from "./assets/project3.png.png";
+import project4 from "./assets/project4.png.png";
 export const personal = {
   name: "Dorthy Christobel",
   role: "Frontend Developer",
@@ -32,5 +33,23 @@ export const projects = [
     tech: ["React", "API"],
     live: "https://dorthys3.github.io/Maharaja-ice-cream/",
     repo: "https://github.com/dorthys3/Maharaja-ice-cream.git"
+  },
+  {
+    id: 3,
+    title: "PERSONAL PORTFOLIO –With (React JS, Bootstrap)",
+    description: "Built a responsive web app with React.js showcasing my studies and live project demo. Implemented React Router for smooth multi-page navigation and reusable components for scalability. Components were reused across sections for scalability and consistency.Included a contact form with validation using React Hooks.",
+    image: project3,   // different image for project 2
+    tech: ["React", "API"],
+    live: "https://dorthys3.github.io/Maharaja-ice-cream/",
+    repo: "https://github.com/dorthys3/Maharaja-ice-cream.git"
+  },
+  {
+    id: 4,
+    title: "TAMILAN TOURS –With (React JS, Bootstrap, Springboot, MySQL  [Freelance Project] )",
+    description: "Designing a travel booking platform focused on curated tour packages across Tamil Nadu. Currently developing features for itinerary customization, user reviews, and real-time booking. It is a fullstack project using React for frontend, Spring Boot for backend, and MySQL for database management.",
+    image: project4,   // different image for project 2
+    tech: ["React", "API"],
+    live: "#",
+    repo: "#"
   }
 ]
